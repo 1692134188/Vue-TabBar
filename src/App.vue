5d3f1360d36e475af05a26bc4873f70c042d2b8c
unknown <A02985@cyai.com>
2021-06-21 16:25:17 +0800

    4.0 自定义TabBarItem，可以传入 图片和文字

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,30 @@
   <div id="app">
     <tab-bar>
       <template v-slot:itemBar>
-        <div class="tab-bar-item">
-          <img src="./assets/img/tabbar/home.svg" alt="" />
-          <div>首页</div>
-        </div>
-        <div class="tab-bar-item">
-          <img src="./assets/img/tabbar/category.svg" alt="" />
-          <div>分类</div>
-        </div>
-        <div class="tab-bar-item">
-          <img src="./assets/img/tabbar/shopcart.svg" alt="" />
-          <div>购物车</div>
-        </div>
-        <div class="tab-bar-item">
-          <img src="./assets/img/tabbar/profile.svg" alt="" />
-          <div>我的</div>
-        </div>
-        
+        <tab-bar-item>
+          <template v-slot:imgSlot>
+            <img src="./assets/img/tabbar/home.svg" alt="" />
+          </template>
+          <template v-slot:textSlot> 首页</template>
+        </tab-bar-item>
+        <tab-bar-item>
+          <template v-slot:imgSlot>
+            <img src="./assets/img/tabbar/category.svg" alt="" />
+          </template>
+          <template v-slot:textSlot> 分类</template>
+        </tab-bar-item>
+        <tab-bar-item>
+          <template v-slot:imgSlot>
+            <img src="./assets/img/tabbar/shopcart.svg" alt="" />
+          </template>
+          <template v-slot:textSlot> 购物车</template>
+        </tab-bar-item>
+        <tab-bar-item>
+          <template v-slot:imgSlot>
+            <img src="./assets/img/tabbar/profile.svg" alt="" />
+          </template>
+          <template v-slot:textSlot> 我的</template>
+        </tab-bar-item>
       </template>
     </tab-bar>
   </div>
@@ -26,10 +33,12 @@
 
 <script>
 import TabBar from "./components/tabbar/TabBar.vue";
+import TabBarItem from "./components/tabbar/TabBarItem.vue";
 export default {
   name: "App",
   components: {
     TabBar,
+    TabBarItem,
   },
 };
 </script>
--- src/components/tabbar/TabBar.vue
+++ src/components/tabbar/TabBar.vue
@@ -22,18 +22,4 @@ export default {
 
   box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
 }
-.tab-bar-item {
-  flex: 1;
-  text-align: center;
-  height: 49px;
-  font-size: 14px;
-}
-
-.tab-bar-item img {
-  width: 24px;
-  height: 24px;
-  margin-top: 3px;
-  vertical-align: middle;
-  margin-bottom: 2px;
-}
 </style>
