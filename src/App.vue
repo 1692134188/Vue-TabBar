0f1b0adf345ba4dc82bbff4658ba5c61acf413b7
unknown <A02985@cyai.com>
2021-06-21 13:13:06 +0800

    2.0版本 封装tabBar组件，在APP中使用，并且设置相关的样式

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,19 @@
 <template>
   <div id="app">
-    <div class="tab-bar">
-      <div class="tab-bar-item">
-        <img src="./assets/img/tabbar/home.svg" alt="" />
-        <div>首页</div>
-      </div>
-      <div class="tab-bar-item">
-        <img src="./assets/img/tabbar/category.svg" alt="" />
-        <div>分类</div>
-      </div>
-      <div class="tab-bar-item">
-        <img src="./assets/img/tabbar/shopcart.svg" alt="" />
-        <div>购物车</div>
-      </div>
-      <div class="tab-bar-item">
-        <img src="./assets/img/tabbar/profile.svg" alt="" />
-        <div>我的</div>
-      </div>
-    </div>
+    <tab-bar></tab-bar>
   </div>
 </template>
 
 <script>
+import TabBar from "./components/tabbar/TabBar.vue";
 export default {
   name: "App",
+  components: {
+    TabBar,
+  },
 };
 </script>
 
 <style>
-body {
-  padding: 0;
-  margin: 0;
-}
-.tab-bar {
-  display: flex;
-  background-color: #f6f6f6;
-
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
-}
-.tab-bar-item {
-    flex: 1;
-    text-align: center;
-    height: 49px;
-    font-size: 14px;
-  }
-
-  .tab-bar-item img {
-    width: 24px;
-    height: 24px;
-    margin-top: 3px;
-    vertical-align: middle;
-    margin-bottom: 2px;
-  }
+@import "./assets/css/base.css";
 </style>
---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="tab-bar">
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/home.svg" alt="" />
+      <div>首页</div>
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/category.svg" alt="" />
+      <div>分类</div>
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/shopcart.svg" alt="" />
+      <div>购物车</div>
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/profile.svg" alt="" />
+      <div>我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style scoped>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
+}
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
