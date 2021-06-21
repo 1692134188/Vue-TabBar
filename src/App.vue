46ca09532f60678120523c4d7c088179a4509a0c
unknown <A02985@cyai.com>
2021-06-21 10:52:11 +0800

    1.0版本，通过在App.vue中设置div和css布局的方式实现

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="tab-bar">
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/home.svg" alt="" />
+        <div>首页</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/category.svg" alt="" />
+        <div>分类</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/shopcart.svg" alt="" />
+        <div>购物车</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/profile.svg" alt="" />
+        <div>我的</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
 }
+.tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
+}
+.tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
 </style>
